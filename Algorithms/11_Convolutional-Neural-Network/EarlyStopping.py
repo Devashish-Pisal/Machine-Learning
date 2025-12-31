8d6512a6545b07cb4bca38b1b257a4f7e445865d
Devashish-Pisal <dpisal85@gmail.com>
2025-12-31 19:16:22 +0100

    EarlyStopping class implemented

---
+++ Algorithms/11_Convolutional-Neural-Network/EarlyStopping.py
@@ -0,0 +1,16 @@
+class EarlyStopping:
+    # One can also implement here the logic for saving the best model, after EarlyStopping is triggered
+    def __init__(self, patience):
+        self.patience = patience
+        self.counter = 0
+        self.best_loss = float('inf')
+        self.early_stop = False
+
+    def __call__(self, val_loss):
+        if val_loss < self.best_loss:
+            self.best_loss = val_loss
+            self.counter = 0
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
